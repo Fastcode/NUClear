24c5db7d90ad92f6919a3c058599c3c851847b25
Trent Houliston <trent@houliston.me>
2013-03-25 14:33:52 +1100

    Fixed the every class to use the correct target

--- NUClear/Every.h
+++ NUClear/Every.h
@@ -12,22 +12,16 @@ namespace NUClear {
             Every() {
             }
 
-            static TimeEmitter e;
-
             class TimeSubscriber {
                 ReactorController& reactorController;
                 TimeSubscriber() : reactorController(ReactorControl) {
-                    e.add(std::chrono::duration_cast<std::chrono::nanoseconds>(period(ticks)), [this]() {
+                    reactorController.addEvery(std::chrono::duration_cast<std::chrono::nanoseconds>(period(ticks)), [this]() {
                         this->reactorControl.emit(new Every<ticks, period>());
                     });
                 }
             };
             static TimeSubscriber subscribe;
     };
-
-    // Let there be statics!
-    template <int ticks, class period>
-    TimeEmitter Every<ticks, period>::e;
     
     template <int ticks, class period>
     typename Every<ticks, period>::TimeSubscriber Every<ticks, period>::subscribe;
