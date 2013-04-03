0df15212adf2a7c09b22e9b62ffd189752954311
Trent Houliston <trent@houliston.me>
2013-04-03 19:36:41 +1100

    Added in the member variables for the reaction

--- NUClear/Reaction.h
+++ NUClear/Reaction.h
@@ -1,7 +1,9 @@
 #ifndef NUCLEAR_REACTION_H
 #define NUCLEAR_REACTION_H
 #include <functional>
+#include <chrono>
 namespace NUClear {
+    typedef std::uint64_t reactionId_t;
     class Reaction {
         public:
             Reaction(std::function<void ()> callback);
@@ -9,6 +11,10 @@ namespace NUClear {
 
             void operator()();
         private:
+            static std::atomic<std::uint64_t> reactionIdSource;
+            reactionId_t m_reactionId;
+            std::uint64_t m_invocations;
+            std::chrono::nanoseconds m_averageRuntime;
             std::function<void ()> m_callback;
     };
 }
