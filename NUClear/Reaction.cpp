48450f08eafd893ff03eb3ce3cc94afc48aaeb93
Trent Houliston <trent@houliston.me>
2013-04-03 20:00:26 +1100

    Added statistics tracking to the system so that it can time reactions

--- NUClear/Reaction.cpp
+++ NUClear/Reaction.cpp
@@ -1,5 +1,5 @@
 #include "Reaction.h"
-
+#include <iostream>
 namespace NUClear {
 
     Reaction::Reaction(std::function<void ()> callback) :
@@ -10,7 +10,14 @@ namespace NUClear {
     Reaction::~Reaction() {}
 
     void Reaction::operator()() {
+        
+        const auto& start = std::chrono::steady_clock::now();
         m_callback();
+        const auto& end = std::chrono::steady_clock::now();
+        
+        const std::chrono::nanoseconds& duration = end.time_since_epoch() - start.time_since_epoch();
+        
+        std::cout << duration.count() << std::endl;
     }
 
 }
