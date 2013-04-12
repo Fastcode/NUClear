d6e65caa130d3ba13ce86c7cf63b2b966f9fbbda
Trent Houliston <trent@houliston.me>
2013-04-12 20:25:01 +1000

    Added some comments to the magic unpack

--- NUClear/Internal/Magic/unpack.h
+++ NUClear/Internal/Magic/unpack.h
@@ -3,6 +3,21 @@
 namespace NUClear {
 namespace Internal {
 namespace Magic {
+    /**
+     * @brief This function is used to execute a series of function calls from a variardic template pack. 
+     * @detail 
+     *  This function is to be used as a helper to expand a variardic template pack into a series of function calls.
+     *  As the variardic function pack can only be expanded in the situation where they are comma seperated (and the
+     *  comma is a syntactic seperator not the comma operator) the only place this can expand is within a function call.
+     *  This function serves that purpose by allowing a series of function calls to be expanded as it's parameters. This
+     *  will execute each of them without having to recursivly evaluate the pack. e.g.
+     *      unpack(function(TType)...);
+     *
+     *  In the case where the return type of the functions is void, then using the comma operator will allow the functions
+     *  to be run e.g.
+     *      unpack((function(TType), 0)...);
+     *  
+     */
     template <typename... Ts>
     void unpack(Ts...) {}
 }
