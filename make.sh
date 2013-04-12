95a34c1de6765f7ceab2b35c8299aa2de64f81a8
Jake Woods <Vodurden@gmail.com>
2013-04-12 16:04:11 +1000

    Made compiling pedantic

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-g++ *.cpp NUClear/*.cpp NUClear/Internal/*.cpp -std=c++11 -pthread -g -Wall -o main
+g++ *.cpp NUClear/*.cpp NUClear/Internal/*.cpp -std=c++11 -pthread -pedantic -g -Wall -o main
