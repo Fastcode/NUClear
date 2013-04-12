34cb3f7f51659e25432077cdcfde705f58cf9ac8
Jake Woods <Vodurden@gmail.com>
2013-04-12 16:00:36 +1000

    Added header guards

--- NUClear/Internal/Magic/unpack.h
+++ NUClear/Internal/Magic/unpack.h
@@ -1,8 +1,11 @@
+#ifndef NUCLEAR_INTERNAL_MAGIC_UNPACK_H
+#define NUCLEAR_INTERNAL_MAGIC_UNPACK_H
 namespace NUClear {
-    namespace Internal {
-        namespace Magic {
-            template <typename... Ts>
-            void unpack(Ts...) {}
-        }
-    }
+namespace Internal {
+namespace Magic {
+    template <typename... Ts>
+    void unpack(Ts...) {}
 }
+}
+}
+#endif
