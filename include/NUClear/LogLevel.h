4ca5d361fa45392c912e190f28511c616bcf45df
Jake Woods <vodurden@gmail.com>
2013-11-25 15:25:22 +1100

    Added proper logging levels

---
+++ include/NUClear/LogLevel.h
@@ -0,0 +1,13 @@
+#ifndef NUCLEAR_LOGLEVEL_H
+#define NUCLEAR_LOGLEVEL_H
+namespace NUClear {
+    enum LogLevel {
+        TRACE,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        FATAL
+    };
+}
+#endif
