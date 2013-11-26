c3574cc105bc424ac366ad3e03c8f7952e9c826e
Trent Houliston <trent@houliston.me>
2013-11-27 01:05:17 +1100

    Split the DSL up into seperate files for easier use. Also disallowed the use of data within the DSL (they must be descriptors only)

---
+++ src/include/nuclear_bits/dsl/Scope.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2013 Jake Woods <jake.f.woods@gmail.com>, Trent Houliston <trent@houliston.me>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef NUCLEAR_DSL_SINGLE_H
+#define NUCLEAR_DSL_SINGLE_H
+
+namespace NUClear {
+    namespace dsl {
+        
+        /**
+         * @brief This enum is used to modify in what way a packet is sent, either Local, Network or both
+         *
+         * @details
+         *  When using the emit function, these options can be added in order to specify where the packet is to be
+         *  sent. If nothing or LOCAL is specified, then it will only be send within this powerplant. If NETWORK is
+         *  specified, then the data will be sent to other powerplants but not this one, if both LOCAL and NETWORK
+         *  are chosen, then the event will propagate to both the network and local powerplants.
+         */
+        struct Scope {
+            struct DIRECT;
+            struct LOCAL;
+            struct NETWORK;
+            struct INITIALIZE;
+        };
+    }
+}
+
+#endif
---
+++ src/include/nuclear_bits/dsl/With.h
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2013 Jake Woods <jake.f.woods@gmail.com>, Trent Houliston <trent@houliston.me>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef NUCLEAR_DSL_WITH_H
+#define NUCLEAR_DSL_WITH_H
+
+namespace NUClear {
+    namespace dsl {
+        
+        /**
+         * @ingroup Wrappers
+         * @brief This is a wrapper class which is used to list the data types to trigger a callback on.
+         *
+         * @details
+         *  This class is used in the on binding to specify additional data which is used in a call, but does not
+         *  trigger the call to run. When the call is run, the latest version of the data available will be obtained.
+         *  from the cache, and used as a paramter in this function call.
+         *
+         * @tparam TWiths the datatypes to get from the cache and use in the callback.
+         */
+        template <typename... TWiths>
+        struct With {
+            With() = delete;
+            ~With() = delete;
+        };
+    }
+}
+
+#endif
