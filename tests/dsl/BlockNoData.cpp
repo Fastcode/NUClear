0bd5bc9ca6a82ff57deaf5331fef6d677f09d81d
Trent Houliston <trent@houliston.me>
2015-04-09 15:42:39 +0700

    Added two new tests. First to check that check that flag types are dealt with properly (since they may be optimised in the future) Second to check that when there is no data available a reaction won't run

--- tests/dsl/BlockNoData.cpp
+++ tests/dsl/BlockNoData.cpp
@@ -20,35 +20,46 @@
 #include "nuclear"
 
 namespace {
-    struct SimpleMessage {
-        int data;
-    };
+    struct SimpleMessage {};
+    
+    struct MessageA {};
+    struct MessageB {};
+    
+    MessageA* a = nullptr;
+    MessageB* b = nullptr;
     
     class TestReactor : public NUClear::Reactor {
     public:
         TestReactor(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
             
-            on<Trigger<SimpleMessage>>().then([this](const SimpleMessage& message) {
+            
+            on<Trigger<SimpleMessage>>().then([this] {
+                auto data = std::make_unique<MessageA>();
+                a = data.get();
                 
-                // The message we received should have test == 10
-                REQUIRE(message.data == 10);
+                // Emit required data
+                emit(std::move(data));
                 
-                // We are finished the test
-                this->powerplant.shutdown();
+                // Since the data was emitted before the shutdown call it will be processed before total shutdown
+                powerplant.shutdown();
+            });
+            
+            on<Trigger<MessageA>, With<MessageB>>().then([this] (const MessageA&, const MessageB&) {
+                FAIL("B was never emitted so this should not be possible");
             });
         }
     };
 }
 
 
-TEST_CASE("A very basic test for Emit and On", "[api][trigger]") {
+TEST_CASE("Testing that when a trigger does not have it's data satisfied it does not run", "[api][nodata]") {
     
     NUClear::PowerPlant::Configuration config;
     config.threadCount = 1;
     NUClear::PowerPlant plant(config);
     plant.install<TestReactor>();
     
-    auto message = std::make_unique<SimpleMessage>(SimpleMessage { 10 });
+    auto message = std::make_unique<SimpleMessage>();
     
     plant.emit(message);
     
