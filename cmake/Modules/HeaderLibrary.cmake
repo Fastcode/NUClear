0f2113678161c232ab8ea686bb0491c9f84f6d38
Trent Houliston <trent@houliston.me>
2018-11-15 12:53:33 +1100

    Improve cmake and add cmake config files (#28)

--- cmake/Modules/HeaderLibrary.cmake
+++ cmake/Modules/HeaderLibrary.cmake
@@ -2,8 +2,8 @@
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
 # Software.
@@ -13,64 +13,69 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-INCLUDE(CMakeParseArguments)
-FUNCTION(HeaderLibrary)
-    # Extract the arguments from our function call
-    SET(options, "")
-    SET(oneValueArgs "NAME")
-    SET(multiValueArgs "HEADER" "PATH_SUFFIX" "URL")
-    CMAKE_PARSE_ARGUMENTS(PACKAGE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+include(CMakeParseArguments)
+function(header_library)
+  # Extract the arguments from our function call
+  set(options, "")
+  set(oneValueArgs "NAME")
+  set(multiValueArgs "HEADER" "PATH_SUFFIX" "URL")
+  cmake_parse_arguments(PACKAGE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    # Clear our required_vars variable
-    UNSET(required_vars)
-    SET(OUTPUT_DIR "${CMAKE_BINARY_DIR}/include")
+  # Clear our required_vars variable
+  unset(required_vars)
+  set(OUTPUT_DIR "${CMAKE_BINARY_DIR}/include")
 
-    # Find our include path
-    FIND_PATH("${PACKAGE_NAME}_INCLUDE_DIR"
-              NAMES ${PACKAGE_HEADER}
-              DOC "The ${PACKAGE_NAME} include directory"
-              PATHS "${OUTPUT_DIR}"
-              PATH_SUFFIXES ${PACKAGE_PATH_SUFFIX}
-    )
- 
-    # File doesn't exist in standard search paths, download it
-    IF(NOT ${PACKAGE_NAME}_INCLUDE_DIR)
+  # Find our include path
+  find_path(
+    "${PACKAGE_NAME}_INCLUDE_DIR"
+    NAMES ${PACKAGE_HEADER}
+    DOC "The ${PACKAGE_NAME} include directory"
+    PATHS "${OUTPUT_DIR}"
+    PATH_SUFFIXES ${PACKAGE_PATH_SUFFIX}
+  )
 
-        # Create the output folder if it doesn't already exist
-        IF(NOT EXISTS "${OUTPUT_DIR}")
-            FILE(MAKE_DIRECTORY "${OUTPUT_DIR}")
-        ENDIF()
+  # File doesn't exist in standard search paths, download it
+  if(NOT ${PACKAGE_NAME}_INCLUDE_DIR)
 
-        # Download file.
-        FILE(DOWNLOAD "${PACKAGE_URL}" "${OUTPUT_DIR}/${PACKAGE_HEADER}" STATUS ${PACKAGE_NAME}_STATUS)
+    # Create the output folder if it doesn't already exist
+    if(NOT EXISTS "${OUTPUT_DIR}")
+      file(MAKE_DIRECTORY "${OUTPUT_DIR}")
+    endif()
 
-        LIST(GET ${PACKAGE_NAME}_STATUS 0 ${PACKAGE_NAME}_STATUS_CODE)
+    # Download file.
+    file(DOWNLOAD "${PACKAGE_URL}" "${OUTPUT_DIR}/${PACKAGE_HEADER}" STATUS ${PACKAGE_NAME}_STATUS)
 
-        # Parse download status
-        IF(${PACKAGE_NAME}_STATUS_CODE EQUAL 0)
-            MESSAGE(STATUS "Successfully downloaded ${PACKAGE_NAME} library.")
+    list(GET ${PACKAGE_NAME}_STATUS 0 ${PACKAGE_NAME}_STATUS_CODE)
 
-            SET(${PACKAGE_NAME}_INCLUDE_DIR "${OUTPUT_DIR}")
+    # Parse download status
+    if(${PACKAGE_NAME}_STATUS_CODE EQUAL 0)
+      message(STATUS "Successfully downloaded ${PACKAGE_NAME} library.")
 
-        ELSE()
-            MESSAGE(ERROR "Failed to download ${PACKAGE_NAME} library.")
-        ENDIF()
-    ENDIF()
+      set(${PACKAGE_NAME}_INCLUDE_DIR "${OUTPUT_DIR}")
 
-    # Setup and export our variables
-    SET(required_vars ${required_vars} "${PACKAGE_NAME}_INCLUDE_DIR")
-    SET(${PACKAGE_NAME}_INCLUDE_DIRS ${${PACKAGE_NAME}_INCLUDE_DIR} PARENT_SCOPE)
-    MARK_AS_ADVANCED(${PACKAGE_NAME}_INCLUDE_DIR ${PACKAGE_NAME}_INCLUDE_DIRS)
+    else()
+      message(ERROR "Failed to download ${PACKAGE_NAME} library.")
+    endif()
+  endif()
 
-    # Find the package
-    INCLUDE(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(${PACKAGE_NAME}
-                                      FOUND_VAR ${PACKAGE_NAME}_FOUND
-                                      REQUIRED_VARS ${required_vars}
-                                      VERSION_VAR ${PACKAGE_NAME}_VERSION
-    )
+  # Setup and export our variables
+  set(required_vars ${required_vars} "${PACKAGE_NAME}_INCLUDE_DIR")
+  set(${PACKAGE_NAME}_INCLUDE_DIRS ${${PACKAGE_NAME}_INCLUDE_DIR} PARENT_SCOPE)
+  mark_as_advanced(${PACKAGE_NAME}_INCLUDE_DIR ${PACKAGE_NAME}_INCLUDE_DIRS)
 
-    # Export our found variable to parent scope
-    SET(${PACKAGE_NAME}_FOUND ${PACKAGE_NAME}_FOUND PARENT_SCOPE)
+  # Find the package
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(
+    ${PACKAGE_NAME}
+    FOUND_VAR
+    ${PACKAGE_NAME}_FOUND
+    REQUIRED_VARS
+    ${required_vars}
+    VERSION_VAR
+    ${PACKAGE_NAME}_VERSION
+  )
 
-ENDFUNCTION(HeaderLibrary)
+  # Export our found variable to parent scope
+  set(${PACKAGE_NAME}_FOUND ${PACKAGE_NAME}_FOUND PARENT_SCOPE)
+
+endfunction(header_library)
--- cmake/Modules/ToolchainLibraryFinder.cmake
+++ cmake/Modules/ToolchainLibraryFinder.cmake
@@ -1,10 +1,10 @@
-# Copyright (C) 2013      Trent Houliston <trent@houliston.me>, Jake Woods <jake.f.woods@gmail.com>
-#               2014-2017 Trent Houliston <trent@houliston.me>
+# Copyright (C) 2013      Trent Houliston <trent@houliston.me>, Jake Woods <jake.f.woods@gmail.com> 2014-2017 Trent
+# Houliston <trent@houliston.me>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
 # Software.
@@ -14,100 +14,116 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-INCLUDE(CMakeParseArguments)
-FUNCTION(ToolchainLibraryFinder)
+include(CMakeParseArguments)
+function(ToolchainLibraryFinder)
 
-    # Extract the arguments from our function call
-    SET(options, "")
-    SET(oneValueArgs "NAME")
-    SET(multiValueArgs "HEADER" "LIBRARY" "PATH_SUFFIX" "BINARY" "VERSION_FILE" "VERSION_BINARY_ARGUMENTS" "VERSION_REGEX")
-    CMAKE_PARSE_ARGUMENTS(PACKAGE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  # Extract the arguments from our function call
+  set(options, "")
+  set(oneValueArgs "NAME")
+  set(
+    multiValueArgs
+    "HEADER"
+    "LIBRARY"
+    "PATH_SUFFIX"
+    "BINARY"
+    "VERSION_FILE"
+    "VERSION_BINARY_ARGUMENTS"
+    "VERSION_REGEX"
+  )
+  cmake_parse_arguments(PACKAGE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    # Clear our required_vars variable
-    UNSET(required_vars)
+  # Clear our required_vars variable
+  unset(required_vars)
 
-    # Find our include path from our named headers
-    IF(PACKAGE_HEADER)
+  # Find our include path from our named headers
+  if(PACKAGE_HEADER)
 
-        # Find our include path
-        FIND_PATH("${PACKAGE_NAME}_INCLUDE_DIR"
-                  NAMES ${PACKAGE_HEADER}
-                  DOC "The ${PACKAGE_NAME} (${PACKAGE_LIBRARY}) include directory"
-                  PATH_SUFFIXES ${PACKAGE_PATH_SUFFIX}
-        )
-
-        # Setup and export our variables
-        SET(required_vars ${required_vars} "${PACKAGE_NAME}_INCLUDE_DIR")
-        SET(${PACKAGE_NAME}_INCLUDE_DIRS ${${PACKAGE_NAME}_INCLUDE_DIR} PARENT_SCOPE)
-        MARK_AS_ADVANCED(${PACKAGE_NAME}_INCLUDE_DIR ${PACKAGE_NAME}_INCLUDE_DIRS)
-
-    ENDIF(PACKAGE_HEADER)
-
-    # Find our library from the named library files
-    IF(PACKAGE_LIBRARY)
-        FIND_LIBRARY("${PACKAGE_NAME}_LIBRARY"
-                     NAMES ${PACKAGE_LIBRARY}
-                     PATH_SUFFIXES ${PACKAGE_PATH_SUFFIX}
-                     DOC "The ${PACKAGE_NAME} (${PACKAGE_LIBRARY}) library"
-        )
-
-        # Setup and export our variables
-        SET(required_vars ${required_vars} "${PACKAGE_NAME}_LIBRARY")
-        SET(${PACKAGE_NAME}_LIBRARIES ${${PACKAGE_NAME}_LIBRARY} PARENT_SCOPE)
-        MARK_AS_ADVANCED(${PACKAGE_NAME}_LIBRARY ${PACKAGE_NAME}_LIBRARIES)
-
-    ENDIF(PACKAGE_LIBRARY)
-
-    # Find our binary from the named binary files
-    IF(PACKAGE_BINARY)
-        FIND_PROGRAM("${PACKAGE_NAME}_BINARY"
-                     NAMES ${PACKAGE_BINARY}
-                     PATH_SUFFIXES ${PACKAGE_PATH_SUFFIX}
-                     DOC "The ${PACKAGE_NAME} (${PACKAGE_BINARY}) executable prgram"
-        )
-
-        # Setup and export our variables
-        SET(required_vars ${required_vars} "${PACKAGE_NAME}_BINARY")
-        SET(${PACKAGE_NAME}_BINARY ${${PACKAGE_NAME}_BINARY} PARENT_SCOPE)
-        MARK_AS_ADVANCED(${PACKAGE_NAME}_BINARY)
-
-    ENDIF(PACKAGE_BINARY)
-
-    # Find our version if we can
-    IF((PACKAGE_VERSION_FILE AND PACKAGE_HEADER) OR (PACKAGE_VERSION_BINARY_ARGUMENTS AND PACKAGE_BINARY))
-        UNSET(full_version_string)
-
-        # Read our package version file into a variable
-        IF(PACKAGE_VERSION_FILE AND PACKAGE_HEADER)
-            FILE(READ "${${PACKAGE_NAME}_INCLUDE_DIR}/${PACKAGE_VERSION_FILE}" full_version_string)
-        ENDIF(PACKAGE_VERSION_FILE AND PACKAGE_HEADER)
-
-        # Execute our binary to get a version string
-        IF(PACKAGE_VERSION_BINARY_ARGUMENTS AND PACKAGE_BINARY)
-            EXEC_PROGRAM(${${PACKAGE_NAME}_BINARY}
-                         ARGS ${PACKAGE_VERSION_BINARY_ARGUMENTS}
-                         OUTPUT_VARIABLE full_version_string)
-        ENDIF(PACKAGE_VERSION_BINARY_ARGUMENTS AND PACKAGE_BINARY)
-
-        # Build up our version string
-        SET(${PACKAGE_NAME}_VERSION "")
-        FOREACH(regex ${PACKAGE_VERSION_REGEX})
-            STRING(REGEX REPLACE ".*${regex}.*" "\\1" regex_output ${full_version_string})
-            SET(${PACKAGE_NAME}_VERSION ${${PACKAGE_NAME}_VERSION} ${regex_output})
-        ENDFOREACH(regex)
-        STRING(REPLACE ";" "." ${PACKAGE_NAME}_VERSION "${${PACKAGE_NAME}_VERSION}")
-
-    ENDIF((PACKAGE_VERSION_FILE AND PACKAGE_HEADER) OR (PACKAGE_VERSION_BINARY_ARGUMENTS AND PACKAGE_BINARY))
-
-    INCLUDE(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(${PACKAGE_NAME}
-                                      FOUND_VAR ${PACKAGE_NAME}_FOUND
-                                      REQUIRED_VARS ${required_vars}
-                                      VERSION_VAR ${PACKAGE_NAME}_VERSION
-                                      #VERSION_VAR "${MAJOR}.${MINOR}.${PATCH}")
+    # Find our include path
+    find_path(
+      "${PACKAGE_NAME}_INCLUDE_DIR"
+      NAMES ${PACKAGE_HEADER}
+      DOC "The ${PACKAGE_NAME} (${PACKAGE_LIBRARY}) include directory"
+      PATH_SUFFIXES ${PACKAGE_PATH_SUFFIX}
     )
 
-    # Export our found variable to parent scope
-    SET(${PACKAGE_NAME}_FOUND ${PACKAGE_NAME}_FOUND PARENT_SCOPE)
+    # Setup and export our variables
+    set(required_vars ${required_vars} "${PACKAGE_NAME}_INCLUDE_DIR")
+    set(${PACKAGE_NAME}_INCLUDE_DIRS ${${PACKAGE_NAME}_INCLUDE_DIR} PARENT_SCOPE)
+    mark_as_advanced(${PACKAGE_NAME}_INCLUDE_DIR ${PACKAGE_NAME}_INCLUDE_DIRS)
 
-ENDFUNCTION(ToolchainLibraryFinder)
+  endif(PACKAGE_HEADER)
+
+  # Find our library from the named library files
+  if(PACKAGE_LIBRARY)
+    find_library(
+      "${PACKAGE_NAME}_LIBRARY"
+      NAMES ${PACKAGE_LIBRARY}
+      PATH_SUFFIXES ${PACKAGE_PATH_SUFFIX}
+      DOC "The ${PACKAGE_NAME} (${PACKAGE_LIBRARY}) library"
+    )
+
+    # Setup and export our variables
+    set(required_vars ${required_vars} "${PACKAGE_NAME}_LIBRARY")
+    set(${PACKAGE_NAME}_LIBRARIES ${${PACKAGE_NAME}_LIBRARY} PARENT_SCOPE)
+    mark_as_advanced(${PACKAGE_NAME}_LIBRARY ${PACKAGE_NAME}_LIBRARIES)
+
+  endif(PACKAGE_LIBRARY)
+
+  # Find our binary from the named binary files
+  if(PACKAGE_BINARY)
+    find_program(
+      "${PACKAGE_NAME}_BINARY"
+      NAMES ${PACKAGE_BINARY}
+      PATH_SUFFIXES ${PACKAGE_PATH_SUFFIX}
+      DOC "The ${PACKAGE_NAME} (${PACKAGE_BINARY}) executable prgram"
+    )
+
+    # Setup and export our variables
+    set(required_vars ${required_vars} "${PACKAGE_NAME}_BINARY")
+    set(${PACKAGE_NAME}_BINARY ${${PACKAGE_NAME}_BINARY} PARENT_SCOPE)
+    mark_as_advanced(${PACKAGE_NAME}_BINARY)
+
+  endif(PACKAGE_BINARY)
+
+  # Find our version if we can
+  if((PACKAGE_VERSION_FILE AND PACKAGE_HEADER) OR (PACKAGE_VERSION_BINARY_ARGUMENTS AND PACKAGE_BINARY))
+    unset(full_version_string)
+
+    # Read our package version file into a variable
+    if(PACKAGE_VERSION_FILE AND PACKAGE_HEADER)
+      file(READ "${${PACKAGE_NAME}_INCLUDE_DIR}/${PACKAGE_VERSION_FILE}" full_version_string)
+    endif(PACKAGE_VERSION_FILE AND PACKAGE_HEADER)
+
+    # Execute our binary to get a version string
+    if(PACKAGE_VERSION_BINARY_ARGUMENTS AND PACKAGE_BINARY)
+      exec_program(
+        ${${PACKAGE_NAME}_BINARY} ARGS ${PACKAGE_VERSION_BINARY_ARGUMENTS} OUTPUT_VARIABLE full_version_string
+      )
+    endif(PACKAGE_VERSION_BINARY_ARGUMENTS AND PACKAGE_BINARY)
+
+    # Build up our version string
+    set(${PACKAGE_NAME}_VERSION "")
+    foreach(regex ${PACKAGE_VERSION_REGEX})
+      string(REGEX REPLACE ".*${regex}.*" "\\1" regex_output ${full_version_string})
+      set(${PACKAGE_NAME}_VERSION ${${PACKAGE_NAME}_VERSION} ${regex_output})
+    endforeach(regex)
+    string(REPLACE ";" "." ${PACKAGE_NAME}_VERSION "${${PACKAGE_NAME}_VERSION}")
+
+  endif((PACKAGE_VERSION_FILE AND PACKAGE_HEADER) OR (PACKAGE_VERSION_BINARY_ARGUMENTS AND PACKAGE_BINARY))
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(
+    ${PACKAGE_NAME}
+    FOUND_VAR
+    ${PACKAGE_NAME}_FOUND
+    REQUIRED_VARS
+    ${required_vars}
+    VERSION_VAR
+    ${PACKAGE_NAME}_VERSION
+    # VERSION_VAR "${MAJOR}.${MINOR}.${PATCH}")
+  )
+
+  # Export our found variable to parent scope
+  set(${PACKAGE_NAME}_FOUND ${PACKAGE_NAME}_FOUND PARENT_SCOPE)
+
+endfunction(ToolchainLibraryFinder)
