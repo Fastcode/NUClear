3ad831bee1060170d537e0c2c4ec4fd51845dd7c
Aaron Wong <aaron.wong@uon.edu.au>
2021-03-26 16:22:45 +1100

    Removed name from the maintainers.

--- src/dsl/word/Once.hpp
+++ src/dsl/word/Once.hpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2013      Trent Houliston <trent@houliston.me>, Jake Woods <jake.f.woods@gmail.com>
  *               2014-2017 Trent Houliston <trent@houliston.me>
- *               2021      Trent Houliston <trent@houliston.me>, Aaron Wong <aaron.wong@live.com.au>
+ *               2021      Trent Houliston <trent@houliston.me>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
