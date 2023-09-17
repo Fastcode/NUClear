639088c930fb5a27c23db35aecd81f7ef5d413d9
Trent Houliston <trent@houliston.me>
2023-09-17 20:32:21 +1000

    Update base license and add a script to update the rest

---
+++ set_license.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Loop through all the files from git ls-files
+for file in $(git ls-files); do
+    # Get the year this file was added
+    year=$(git log --diff-filter=A --follow --format=%aD ${file} | tail -1 | awk '{print $4}')
+    echo ${file} ${year}
+    licenseheaders -t .licence.tmpl --years="${year}" --owner "NUClear Contributors" --projname "NUClear" --projurl="https://github.com/Fastcode/NUClear"
+done
