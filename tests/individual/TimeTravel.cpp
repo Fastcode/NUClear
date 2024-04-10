64158d7a8f510514f66c4a7b3eec51c49a9adaa2
Tom0Brien <thomas16441@gmail.com>
2024-04-10 15:24:38 +1000

    Clean up test

--- tests/individual/TimeTravel.cpp
+++ tests/individual/TimeTravel.cpp
@@ -6,7 +6,20 @@
 
 namespace {
 
-constexpr NUClear::clock::duration time_unit = std::chrono::milliseconds(10);
+using TestUnits                       = std::chrono::duration<int64_t, std::ratio<1, 50>>;
+static constexpr int64_t EVENT_1_TIME = 4;
+static constexpr int64_t EVENT_2_TIME = 8;
+
+struct Results {
+    struct TimePair {
+        NUClear::clock::time_point nuclear;
+        std::chrono::steady_clock::time_point steady;
+    };
+
+    TimePair start;
+    TimePair zero;
+    std::array<TimePair, 2> events;
+};
 
 class TestReactor : public test_util::TestBase<TestReactor, 5000> {
 public:
@@ -22,39 +35,37 @@ public:
     // Real-time factor
     double rtf = 1.0;
 
-    // Expected results = {steady_delta_task_1, steady_delta_task_2}
-    std::array<NUClear::clock::duration, 2> results = {std::chrono::milliseconds(0), std::chrono::milliseconds(0)};
+    // Results struct
+    Results results;
 
     TestReactor(std::unique_ptr<NUClear::Environment> environment) : TestBase(std::move(environment), false) {
 
         on<Startup>().then([this] {
-            steady_start_time = std::chrono::steady_clock::now();
+            results.zero = Results::TimePair{NUClear::clock::now(), std::chrono::steady_clock::now()};
 
-            // Emit a chrono task to run at time 2
-            auto chrono_task_1 = std::make_unique<NUClear::dsl::operation::ChronoTask>(
+            // Emit a chrono task to run at time EVENT_1_TIME
+            emit<Scope::DIRECT>(std::make_unique<NUClear::dsl::operation::ChronoTask>(
                 [this](NUClear::clock::time_point&) {
-                    results[0] = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()
-                                                                                       - steady_start_time);
+                    results.events[0] = Results::TimePair{NUClear::clock::now(), std::chrono::steady_clock::now()};
                     return false;
                 },
-                NUClear::clock::now() + 2 * time_unit,
-                1);
-            emit<Scope::DIRECT>(std::move(chrono_task_1));
+                NUClear::clock::now() + TestUnits(EVENT_1_TIME),
+                1));
 
-            // Emit a chrono task to run at time 4, and shutdown
-            auto chrono_task_2 = std::make_unique<NUClear::dsl::operation::ChronoTask>(
+            // Emit a chrono task to run at time EVENT_2_TIME, and shutdown
+            emit<Scope::DIRECT>(std::make_unique<NUClear::dsl::operation::ChronoTask>(
                 [this](NUClear::clock::time_point&) {
-                    results[1] = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()
-                                                                                       - steady_start_time);
+                    results.events[1] = Results::TimePair{NUClear::clock::now(), std::chrono::steady_clock::now()};
                     powerplant.shutdown();
                     return false;
                 },
-                NUClear::clock::now() + 4 * time_unit,
-                1);
-            emit<Scope::DIRECT>(std::move(chrono_task_2));
+                NUClear::clock::now() + TestUnits(EVENT_2_TIME),
+                2));
 
             // Time travel!
             emit<Scope::DIRECT>(std::make_unique<NUClear::message::TimeTravel>(adjustment, rtf, action));
+
+            results.start = Results::TimePair{NUClear::clock::now(), std::chrono::steady_clock::now()};
         });
     }
 };
@@ -62,9 +73,7 @@ public:
 }  // anonymous namespace
 
 
-// TEST_CASE("Test time travel correctly changes the time for zero rtf", "[time_travel][chrono_controller]") {
-
-// }
+TEST_CASE("Test time travel correctly changes the time for zero rtf", "[time_travel][chrono_controller]") {}
 
 TEST_CASE("Test time travel correctly changes the time for non zero rtf", "[time_travel][chrono_controller]") {
 
@@ -75,11 +84,12 @@ TEST_CASE("Test time travel correctly changes the time for non zero rtf", "[time
     auto& reactor = plant->install<TestReactor>();
 
     // Set the reactor fields to the values we want to test
-    auto action        = GENERATE(Action::RELATIVE, Action::ABSOLUTE, Action::NEAREST);
-    auto adjustment    = GENERATE(-2, -1, 0, 1, 2, 3, 4, 5);
-    auto rtf           = GENERATE(0.5, 1.0, 2.0);
+    Action action      = GENERATE(Action::RELATIVE, Action::ABSOLUTE, Action::NEAREST);
+    int64_t adjustment = GENERATE(-4, -2, 0, 2, 4, 6, 8, 10);
+    double rtf         = GENERATE(0.5, 1.0, 2.0);
+    CAPTURE(action, adjustment, rtf);
     reactor.action     = action;
-    reactor.adjustment = adjustment * time_unit;
+    reactor.adjustment = TestUnits(adjustment);
     reactor.rtf        = rtf;
 
     // Reset clock to zero
@@ -88,35 +98,49 @@ TEST_CASE("Test time travel correctly changes the time for non zero rtf", "[time
     // Start the powerplant
     plant->start();
 
-    // Expected results = {steady_delta_task_1, steady_delta_task_2}
-    std::array<NUClear::clock::duration, 2> expected_results;
+    // Expected results
+    std::array<int64_t, 2> expected;
     switch (action) {
-        case Action::RELATIVE:
-            expected_results = {std::chrono::duration_cast<NUClear::clock::duration>((2 * time_unit) / rtf),
-                                std::chrono::duration_cast<NUClear::clock::duration>((4 * time_unit) / rtf)};
-            break;
+        case Action::RELATIVE: expected = {EVENT_1_TIME, EVENT_2_TIME}; break;
         case Action::ABSOLUTE:
-            expected_results = {
-                std::chrono::duration_cast<NUClear::clock::duration>((std::max(0, 2 - adjustment) * time_unit) / rtf),
-                std::chrono::duration_cast<NUClear::clock::duration>((std::max(0, 4 - adjustment) * time_unit) / rtf)};
+            expected = {std::max(0l, EVENT_1_TIME - adjustment), std::max(0l, EVENT_2_TIME - adjustment)};
             break;
         case Action::NEAREST:
-            if (adjustment < 2) {
-                expected_results = {
-                    std::chrono::duration_cast<NUClear::clock::duration>((2 - adjustment) * time_unit / rtf),
-                    std::chrono::duration_cast<NUClear::clock::duration>((4 - adjustment) * time_unit / rtf)};
-            }
-            else if (adjustment >= 2) {
-                expected_results = {std::chrono::milliseconds(0),
-                                    std::chrono::duration_cast<NUClear::clock::duration>((2 * time_unit) / rtf)};
-            }
+            expected = adjustment < EVENT_1_TIME
+                           ? std::array<int64_t, 2>{EVENT_1_TIME - adjustment, EVENT_2_TIME - adjustment}
+                           : std::array<int64_t, 2>{0, EVENT_2_TIME - EVENT_1_TIME};
             break;
+        default: throw std::runtime_error("Unknown action");
     }
 
-    // Check the results
-    INFO("action: " << static_cast<int>(action) << ", adjustment: " << adjustment << ", rtf: " << rtf);
-    INFO("results: " << reactor.results[0].count() << ", " << reactor.results[1].count());
-    INFO("expected: " << expected_results[0].count() << ", " << expected_results[1].count());
-    REQUIRE(std::abs(reactor.results[0].count() - expected_results[0].count()) < time_unit.count());
-    REQUIRE(std::abs(reactor.results[1].count() - expected_results[1].count()) < time_unit.count());
+    std::array<TestUnits, 2> expected_nuclear = {TestUnits(expected[0]), TestUnits(expected[1])};
+    std::array<TestUnits, 2> expected_steady  = {TestUnits(int64_t(expected[0] / rtf)),
+                                                 TestUnits(int64_t(expected[1] / rtf))};
+
+    const auto& r       = reactor.results;
+    const auto& n_start = reactor.results.start.nuclear;
+    const auto& s_start = reactor.results.start.steady;
+
+    auto round_to_test_units = [](const auto& duration) {
+        double d = std::chrono::duration_cast<std::chrono::duration<double>>(duration).count();
+        double t = (TestUnits::period::den * d) / TestUnits::period::num;
+        return TestUnits(std::lround(t));
+    };
+
+    std::array<TestUnits, 2> actual_nuclear = {
+        round_to_test_units(r.events[0].nuclear - n_start),
+        round_to_test_units(r.events[1].nuclear - n_start),
+    };
+    std::array<TestUnits, 2> actual_steady = {
+        round_to_test_units(r.events[0].steady - s_start),
+        round_to_test_units(r.events[1].steady - s_start),
+    };
+
+    TestUnits actual_adjustment(round_to_test_units(r.start.nuclear - r.zero.nuclear));
+    TestUnits expected_adjustment(std::min(adjustment, action == Action::NEAREST ? EVENT_1_TIME : adjustment));
+    CHECK(expected_nuclear[0] == actual_nuclear[0]);
+    CHECK(expected_nuclear[1] == actual_nuclear[1]);
+    CHECK(expected_steady[0] == actual_steady[0]);
+    CHECK(expected_steady[1] == actual_steady[1]);
+    CHECK(expected_adjustment == actual_adjustment);
 }
