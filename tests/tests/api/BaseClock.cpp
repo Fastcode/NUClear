b0b782a14f0e191165204f07f964b2b40a73f8d6
Trent Houliston <trent@houliston.me>
2024-07-14 21:40:01 +0200

    Upgrade to a newer version of Catch using FetchContent (#110)
    Upgrades to a newer version of the Catch unit testing framework.
    Also swaps to using FetchContent to get it

---
+++ tests/tests/api/BaseClock.cpp
@@ -0,0 +1,142 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 NUClear Contributors
+ *
+ * This file is part of the NUClear codebase.
+ * See https://github.com/Fastcode/NUClear for further info.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <catch2/catch_test_macros.hpp>
+#include <ctime>  // for localtime_r/s
+
+// This define declares that we are using steady_clock as the base clock for NUClear
+#define NUCLEAR_CLOCK_TYPE std::chrono::steady_clock
+
+#include <chrono>
+#include <nuclear>
+#include <utility>
+
+#include "message/ReactionStatistics.hpp"
+#include "test_util/TestBase.hpp"
+
+// Anonymous namespace to keep everything file local
+namespace {
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+std::vector<std::pair<NUClear::clock::time_point, std::chrono::steady_clock::time_point>> times;
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+std::mutex times_mutex;
+constexpr int n_time = 100;
+
+class TestReactor : public NUClear::Reactor {
+public:
+    TestReactor(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+
+        // Have a frequently running reaction so that ReactionStatistics will be emitted
+        on<Every<10, std::chrono::milliseconds>>().then([] {});
+
+        // Trigger on ReactionStatistics so that we can record the times that messages were emitted and compare them
+        // against the current time If everything is working as it should then the emitted times and the current times
+        // should be identical (at least down to the second, but it depends on when the clock ticks over)
+        on<Trigger<NUClear::message::ReactionStatistics>>().then(
+            [this](const NUClear::message::ReactionStatistics& stats) {
+                const std::lock_guard<std::mutex> lock(times_mutex);
+                times.push_back(std::make_pair(stats.finished, std::chrono::steady_clock::now()));
+                if (times.size() > n_time) {
+                    powerplant.shutdown();
+                }
+            });
+    }
+};
+}  // namespace
+
+TEST_CASE("Testing base clock works correctly", "[api][base_clock]") {
+
+    INFO("Ensure NUClear base_clock is the correct type");
+    STATIC_REQUIRE(std::is_base_of<std::chrono::steady_clock, NUClear::clock>::value);
+
+    // Construct the powerplant
+    NUClear::Configuration config;
+    config.thread_count = 1;
+    NUClear::PowerPlant plant(config);
+
+    // Install our test reactor
+    plant.install<TestReactor>();
+
+    // Start the powerplant
+    plant.start();
+
+    // Setup a struct to convert std::tm* into a more usable format and to make comparison cleaner
+    struct TimeData {
+        TimeData(const std::tm* tm)
+            : year(tm->tm_year)
+            , month(tm->tm_mon)
+            , day(tm->tm_mday)
+            , hour(tm->tm_hour)
+            , min(tm->tm_min)
+            , sec(tm->tm_sec) {}
+        bool operator==(const TimeData& other) const {
+            return year == other.year && month == other.month && day == other.day && hour == other.hour
+                   && min == other.min && sec == other.sec;
+        }
+        int year;
+        int month;
+        int day;
+        int hour;
+        int min;
+        int sec;
+    };
+
+    // Compute the differences between the time pairs
+    int match_count = 0;
+    for (const auto& time_pairs : times) {
+        using namespace std::chrono;  // NOLINT(google-build-using-namespace)
+        const std::time_t ntt = system_clock::to_time_t(
+            system_clock::time_point(duration_cast<system_clock::duration>(time_pairs.first.time_since_epoch())));
+        const std::time_t stt = system_clock::to_time_t(
+            system_clock::time_point(duration_cast<system_clock::duration>(time_pairs.second.time_since_epoch())));
+
+        std::tm result{};
+#ifdef WIN32
+        localtime_s(&result, &ntt);
+#else
+        localtime_r(&ntt, &result);
+#endif  // WIN32
+        const TimeData nuclear_clock(&result);
+
+#ifdef WIN32
+        localtime_s(&result, &stt);
+#else
+        localtime_r(&stt, &result);
+#endif  // WIN32
+
+        const TimeData local_clock(&result);
+
+        UNSCOPED_INFO("Year.: " << nuclear_clock.year + 1900 << " == " << local_clock.year + 1900 << "\n"
+                                << "Month: " << nuclear_clock.month << " == " << local_clock.month << "\n"
+                                << "Day..: " << nuclear_clock.day << " == " << local_clock.day << "\n"
+                                << "Hour.: " << nuclear_clock.hour << " == " << local_clock.hour << "\n"
+                                << "Min..: " << nuclear_clock.min << " == " << local_clock.min << "\n"
+                                << "Sec..: " << nuclear_clock.sec << " == " << local_clock.sec);
+        if (nuclear_clock == local_clock) {
+            ++match_count;
+        }
+    }
+
+    // At least 95% of all reaction statistics should match to the second
+    REQUIRE(match_count >= 95);
+}
---
+++ tests/tests/api/TimeTravelFrozen.cpp
@@ -0,0 +1,116 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <future>
+#include <nuclear>
+
+#include "test_util/TestBase.hpp"
+
+namespace {
+
+constexpr std::chrono::milliseconds EVENT_1_TIME  = std::chrono::milliseconds(4);
+constexpr std::chrono::milliseconds EVENT_2_TIME  = std::chrono::milliseconds(8);
+constexpr std::chrono::milliseconds SHUTDOWN_TIME = std::chrono::milliseconds(12);
+
+struct WaitForShutdown {};
+
+class TestReactor : public test_util::TestBase<TestReactor, 5000> {
+public:
+    TestReactor(std::unique_ptr<NUClear::Environment> environment) : TestBase(std::move(environment), false) {
+
+        on<Startup>().then([this] {
+            // Reset clock to zero
+            NUClear::clock::set_clock(NUClear::clock::time_point(), 0.0);
+
+            // Emit a chrono task to run at time EVENT_1_TIME
+            emit<Scope::DIRECT>(std::make_unique<NUClear::dsl::operation::ChronoTask>(
+                [this](NUClear::clock::time_point&) {
+                    events.push_back("Event 1");
+                    return false;
+                },
+                NUClear::clock::time_point(EVENT_1_TIME),
+                1));
+
+            // Emit a chrono task to run at time EVENT_2_TIME
+            emit<Scope::DIRECT>(std::make_unique<NUClear::dsl::operation::ChronoTask>(
+                [this](NUClear::clock::time_point&) {
+                    events.push_back("Event 2");
+                    return false;
+                },
+                NUClear::clock::time_point(EVENT_2_TIME),
+                2));
+
+            // Time travel
+            emit<Scope::DIRECT>(
+                std::make_unique<NUClear::message::TimeTravel>(NUClear::clock::time_point(adjustment), rtf, action));
+
+            // Shutdown after steady clock amount of time
+            emit(std::make_unique<WaitForShutdown>());
+        });
+
+        on<Trigger<WaitForShutdown>>().then([this] {
+            std::this_thread::sleep_for(SHUTDOWN_TIME);
+            events.push_back("Finished");
+            powerplant.shutdown();
+        });
+    }
+
+    // Time travel action
+    NUClear::message::TimeTravel::Action action = NUClear::message::TimeTravel::Action::RELATIVE;
+
+    // Time adjustment
+    NUClear::clock::duration adjustment = std::chrono::milliseconds(0);
+
+    // Real-time factor
+    double rtf = 1.0;
+
+    // Events
+    std::vector<std::string> events = {};
+};
+
+}  // anonymous namespace
+
+TEST_CASE("Test time travel correctly changes the time for non zero rtf", "[time_travel][chrono_controller]") {
+
+    using Action = NUClear::message::TimeTravel::Action;
+
+    const NUClear::Configuration config;
+    auto plant    = std::make_shared<NUClear::PowerPlant>(config);
+    auto& reactor = plant->install<TestReactor>();
+
+    // Set the reactor fields to the values we want to test
+    const Action action      = GENERATE(Action::RELATIVE, Action::ABSOLUTE, Action::NEAREST);
+    const int64_t adjustment = GENERATE(-4, -2, 0, 2, 4, 6, 8, 10);
+    CAPTURE(action, adjustment);
+    reactor.action     = action;
+    reactor.adjustment = std::chrono::milliseconds(adjustment);
+    reactor.rtf        = 0.0;
+
+    // Start the powerplant
+    plant->start();
+
+    // Expected results
+    std::vector<std::string> expected;
+    switch (action) {
+        case Action::RELATIVE: expected = {"Finished"}; break;
+        case Action::ABSOLUTE:
+            if (std::chrono::milliseconds(adjustment) < EVENT_1_TIME) {
+                expected = {"Finished"};
+            }
+            else if (std::chrono::milliseconds(adjustment) < EVENT_2_TIME) {
+                expected = {"Event 1", "Finished"};
+            }
+            else {
+                expected = {"Event 1", "Event 2", "Finished"};
+            }
+            break;
+        case Action::NEAREST:
+            expected = std::chrono::milliseconds(adjustment) < EVENT_1_TIME
+                           ? std::vector<std::string>{"Finished"}
+                           : std::vector<std::string>{"Event 1", "Finished"};
+            break;
+        default: throw std::runtime_error("Unknown action");
+    }
+
+    INFO(test_util::diff_string(expected, reactor.events));
+    CHECK(expected == reactor.events);
+}
