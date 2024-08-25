c4bfe3457bdf0f304f5e48496a9528fa5d1d7f8c
Trent Houliston <trent@houliston.me>
2024-08-25 11:40:55 +1000

    clang-tidy

--- src/threading/scheduler/Pool.cpp
+++ src/threading/scheduler/Pool.cpp
@@ -142,7 +142,7 @@ namespace threading {
         }
 
         bool Pool::is_idle() const {
-            std::lock_guard<std::mutex> lock(mutex);
+            const std::lock_guard<std::mutex> lock(mutex);
             return pool_idle != nullptr;
         }
 
