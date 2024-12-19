fb376f403a9cfb6d2c8be82b4e824569a8561c6e
Trent Houliston <trent@houliston.me>
2024-12-19 11:07:30 +1100

    Make string_join a utility and add tests

---
+++ tests/tests/util/string_join.cpp
@@ -0,0 +1,121 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 NUClear Contributors
+ *
+ * This file is part of the NUClear codebase.
+ * See https://github.com/Fastcode/NUClear for further info.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "util/string_join.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <string>
+#include <typeinfo>
+
+struct TestSymbol {
+    friend std::ostream& operator<<(std::ostream& os, const TestSymbol&) {
+        return os << typeid(TestSymbol).name();
+    }
+};
+
+
+SCENARIO("Test string_join correctly joins strings", "[util][string_join]") {
+
+    GIVEN("An empty set of arguments") {
+        std::string delimiter = GENERATE("", ",", " ");
+
+        WHEN("string_join is called") {
+            const std::string result = NUClear::util::string_join(delimiter);
+
+            THEN("It should return an empty string") {
+                REQUIRE(result.empty());
+            }
+        }
+    }
+
+    GIVEN("A single string argument") {
+        std::string delimiter = GENERATE("", ",", " ");
+        std::string arg       = GENERATE("test", "string", "join");
+
+        WHEN("string_join is called") {
+            const std::string result = NUClear::util::string_join(delimiter, arg);
+
+            THEN("It should return the argument") {
+                REQUIRE(result == arg);
+            }
+        }
+    }
+
+    GIVEN("Two string arguments") {
+        std::string delimiter = GENERATE("", ",", " ");
+        std::string arg1      = GENERATE("test", "string", "join");
+        std::string arg2      = GENERATE("test", "string", "join");
+
+        WHEN("string_join is called") {
+            const std::string result = NUClear::util::string_join(delimiter, arg1, arg2);
+
+            THEN("It should return the two arguments joined by the delimiter") {
+                REQUIRE(result == arg1 + delimiter + arg2);
+            }
+        }
+    }
+
+    GIVEN("Three string arguments") {
+        std::string delimiter = GENERATE("", ",", " ");
+        std::string arg1      = GENERATE("test", "string", "join");
+        std::string arg2      = GENERATE("test", "string", "join");
+        std::string arg3      = GENERATE("test", "string", "join");
+
+        WHEN("string_join is called") {
+            const std::string result = NUClear::util::string_join(delimiter, arg1, arg2, arg3);
+
+            THEN("It should return the three arguments joined by the delimiter") {
+                REQUIRE(result == arg1 + delimiter + arg2 + delimiter + arg3);
+            }
+        }
+    }
+
+    GIVEN("A mix of string and non-string arguments") {
+        std::string delimiter = GENERATE("", ",", " ");
+        std::string arg1      = GENERATE("test", "string", "join");
+        std::string arg2      = GENERATE("test", "string", "join");
+        int arg3              = GENERATE(1, 2, 3);
+
+        WHEN("string_join is called") {
+            const std::string result = NUClear::util::string_join(delimiter, arg1, arg2, arg3);
+
+            THEN("It should return the arguments joined by the delimiter") {
+                REQUIRE(result == arg1 + delimiter + arg2 + delimiter + std::to_string(arg3));
+            }
+        }
+    }
+
+    GIVEN("A class with an overloaded operator<<") {
+        std::string delimiter = GENERATE("", ",", " ");
+        TestSymbol arg1;
+        TestSymbol arg2;
+
+        WHEN("string_join is called") {
+            const std::string result = NUClear::util::string_join(delimiter, arg1, arg2);
+
+            THEN("It should return the arguments joined by the delimiter") {
+                REQUIRE(result == typeid(TestSymbol).name() + delimiter + typeid(TestSymbol).name());
+            }
+        }
+    }
+}
