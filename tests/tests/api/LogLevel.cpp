82924a16f254220f6575f04a2b63573cf0cec419
Trent Houliston <trent@houliston.me>
2024-12-23 22:29:40 +1100

    clang-tidy

--- tests/tests/api/LogLevel.cpp
+++ tests/tests/api/LogLevel.cpp
@@ -14,7 +14,7 @@ SCENARIO("LogLevel can be constructed from Value") {
                               NUClear::LogLevel::WARN,
                               NUClear::LogLevel::ERROR,
                               NUClear::LogLevel::FATAL);
-        NUClear::LogLevel log_level(value);
+        const NUClear::LogLevel log_level(value);
 
         WHEN("the value is retrieved") {
             auto retrieved_value = log_level();
@@ -28,8 +28,8 @@ SCENARIO("LogLevel can be constructed from Value") {
 
 SCENARIO("LogLevel can be constructed from a string") {
     GIVEN("A LogLevel constructed from a string") {
-        auto str = GENERATE("TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL");
-        NUClear::LogLevel log_level(str);
+        const auto str = GENERATE("TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL");
+        const NUClear::LogLevel log_level(str);
 
         WHEN("the value is retrieved") {
             auto value = log_level();
@@ -43,13 +43,13 @@ SCENARIO("LogLevel can be constructed from a string") {
 
 SCENARIO("LogLevel can be converted to a string") {
     GIVEN("A LogLevel") {
-        auto value = GENERATE(NUClear::LogLevel::TRACE,
-                              NUClear::LogLevel::DEBUG,
-                              NUClear::LogLevel::INFO,
-                              NUClear::LogLevel::WARN,
-                              NUClear::LogLevel::ERROR,
-                              NUClear::LogLevel::FATAL);
-        NUClear::LogLevel log_level(value);
+        const auto value = GENERATE(NUClear::LogLevel::TRACE,
+                                    NUClear::LogLevel::DEBUG,
+                                    NUClear::LogLevel::INFO,
+                                    NUClear::LogLevel::WARN,
+                                    NUClear::LogLevel::ERROR,
+                                    NUClear::LogLevel::FATAL);
+        const NUClear::LogLevel log_level(value);
 
         WHEN("it is converted to a string") {
             std::string str = log_level;
@@ -63,13 +63,13 @@ SCENARIO("LogLevel can be converted to a string") {
 
 SCENARIO("LogLevel can be streamed to an ostream") {
     GIVEN("A LogLevel") {
-        auto value = GENERATE(NUClear::LogLevel::TRACE,
-                              NUClear::LogLevel::DEBUG,
-                              NUClear::LogLevel::INFO,
-                              NUClear::LogLevel::WARN,
-                              NUClear::LogLevel::ERROR,
-                              NUClear::LogLevel::FATAL);
-        NUClear::LogLevel log_level(value);
+        const auto value = GENERATE(NUClear::LogLevel::TRACE,
+                                    NUClear::LogLevel::DEBUG,
+                                    NUClear::LogLevel::INFO,
+                                    NUClear::LogLevel::WARN,
+                                    NUClear::LogLevel::ERROR,
+                                    NUClear::LogLevel::FATAL);
+        const NUClear::LogLevel log_level(value);
 
         WHEN("it is streamed to an ostream") {
             std::ostringstream os;
@@ -84,21 +84,21 @@ SCENARIO("LogLevel can be streamed to an ostream") {
 
 SCENARIO("LogLevel comparison operators work correctly") {
     GIVEN("Two LogLevel enum values") {
-        NUClear::LogLevel::Value v1 = GENERATE(NUClear::LogLevel::TRACE,
-                                               NUClear::LogLevel::DEBUG,
-                                               NUClear::LogLevel::INFO,
-                                               NUClear::LogLevel::WARN,
-                                               NUClear::LogLevel::ERROR,
-                                               NUClear::LogLevel::FATAL);
-        NUClear::LogLevel::Value v2 = GENERATE(NUClear::LogLevel::TRACE,
-                                               NUClear::LogLevel::DEBUG,
-                                               NUClear::LogLevel::INFO,
-                                               NUClear::LogLevel::WARN,
-                                               NUClear::LogLevel::ERROR,
-                                               NUClear::LogLevel::FATAL);
+        const NUClear::LogLevel::Value v1 = GENERATE(NUClear::LogLevel::TRACE,
+                                                     NUClear::LogLevel::DEBUG,
+                                                     NUClear::LogLevel::INFO,
+                                                     NUClear::LogLevel::WARN,
+                                                     NUClear::LogLevel::ERROR,
+                                                     NUClear::LogLevel::FATAL);
+        const NUClear::LogLevel::Value v2 = GENERATE(NUClear::LogLevel::TRACE,
+                                                     NUClear::LogLevel::DEBUG,
+                                                     NUClear::LogLevel::INFO,
+                                                     NUClear::LogLevel::WARN,
+                                                     NUClear::LogLevel::ERROR,
+                                                     NUClear::LogLevel::FATAL);
 
         WHEN("two smart enum value is constructed") {
-            NUClear::LogLevel ll1(v1);
+            const NUClear::LogLevel ll1(v1);
             AND_WHEN("they are compared using ==") {
                 THEN("the result should be correct") {
                     REQUIRE((ll1 == v2) == (v1 == v2));
@@ -132,8 +132,8 @@ SCENARIO("LogLevel comparison operators work correctly") {
         }
 
         WHEN("two smart enum values are constructed") {
-            NUClear::LogLevel ll1(v1);
-            NUClear::LogLevel ll2(v2);
+            const NUClear::LogLevel ll1(v1);
+            const NUClear::LogLevel ll2(v2);
             AND_WHEN("they are compared using ==") {
                 THEN("the result should be correct") {
                     REQUIRE((ll1 == ll2) == (v1 == v2));
@@ -170,13 +170,13 @@ SCENARIO("LogLevel comparison operators work correctly") {
 
 SCENARIO("LogLevel can be used in switch statements") {
     GIVEN("A LogLevel") {
-        auto value = GENERATE(NUClear::LogLevel::TRACE,
-                              NUClear::LogLevel::DEBUG,
-                              NUClear::LogLevel::INFO,
-                              NUClear::LogLevel::WARN,
-                              NUClear::LogLevel::ERROR,
-                              NUClear::LogLevel::FATAL);
-        NUClear::LogLevel log_level(value);
+        const auto value = GENERATE(NUClear::LogLevel::TRACE,
+                                    NUClear::LogLevel::DEBUG,
+                                    NUClear::LogLevel::INFO,
+                                    NUClear::LogLevel::WARN,
+                                    NUClear::LogLevel::ERROR,
+                                    NUClear::LogLevel::FATAL);
+        const NUClear::LogLevel log_level(value);
 
         WHEN("used in a switch statement") {
             std::string result;
