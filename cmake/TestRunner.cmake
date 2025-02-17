4ddf7445b82d8e28884cb8d2d478b556cd3d8751
Trent Houliston <trent@houliston.me>
2025-02-17 20:46:32 +1100

    Do coverage in clang so it can be done in release mode (#175)
    Clang uses a different coverage model which means it can be done in
    release mode.
    This will help a lot with some of the slower CI tests and ensuring
    consistency.

--- cmake/TestRunner.cmake
+++ cmake/TestRunner.cmake
@@ -36,25 +36,68 @@ get_all_catch_test_targets(all_targets ${PROJECT_SOURCE_DIR})
 
 # Create a custom command for each test target to run it
 # Make sure that coverage data is written with paths relative to the source directory
-unset(reports)
+unset(report_outputs)
+unset(coverage_reports)
 foreach(target ${all_targets})
 
-  set(sonarqube_report_file "${PROJECT_BINARY_DIR}/reports/tests/${target}.sonarqube.xml")
+  unset(command_prefix)
+  if(ENABLE_COVERAGE AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    # Extra output files for the profile data
+    set(raw_coverage "${PROJECT_BINARY_DIR}/reports/tests/${target}.profraw")
+    set(indexed_coverage "${PROJECT_BINARY_DIR}/reports/tests/${target}.profdata")
+    set(coverage_report "${PROJECT_BINARY_DIR}/reports/tests/${target}.txt")
+    set(command_prefix "cmake" "-E" "env" "LLVM_PROFILE_FILE=${raw_coverage}")
+    list(APPEND coverage_reports ${coverage_report})
+  endif()
+
   set(junit_report_file "${PROJECT_BINARY_DIR}/reports/tests/${target}.junit.xml")
-  list(APPEND reports ${sonarqube_report_file} ${junit_report_file})
+  list(APPEND report_outputs ${junit_report_file})
   add_custom_command(
-    OUTPUT ${sonarqube_report_file} ${junit_report_file}
-    COMMAND $<TARGET_FILE:${target}> --reporter console --reporter SonarQube::out=${sonarqube_report_file} --reporter
-            JUnit::out=${junit_report_file}
+    OUTPUT ${junit_report_file} ${raw_coverage}
+    COMMAND ${command_prefix} $<TARGET_FILE:${target}> --reporter console --reporter JUnit::out=${junit_report_file}
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    DEPENDS ${target}
     USES_TERMINAL
     COMMENT "Running test ${target}"
   )
+
+  if(ENABLE_COVERAGE AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    add_custom_command(
+      OUTPUT ${indexed_coverage}
+      COMMAND llvm-profdata merge -sparse ${raw_coverage} -o ${indexed_coverage}
+      DEPENDS ${raw_coverage}
+    )
+
+    add_custom_command(
+      OUTPUT ${coverage_report}
+      COMMAND llvm-cov show --show-branches=count -Xdemangler c++filt -instr-profile=${indexed_coverage}
+              $<TARGET_FILE:${target}> > ${coverage_report}
+      DEPENDS ${indexed_coverage}
+    )
+  endif()
+
 endforeach()
 
 # Create a custom target that depends on all test targets
 add_custom_target(
   run_all_tests
-  DEPENDS ${reports}
+  DEPENDS ${report_outputs} ${all_targets}
   COMMENT "Running all Catch2 tests"
 )
+
+# Concatenate all the coverage reports together
+if(ENABLE_COVERAGE AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+
+  add_custom_command(
+    OUTPUT ${PROJECT_BINARY_DIR}/reports/coverage.txt
+    COMMAND cat ${coverage_reports} > ${PROJECT_BINARY_DIR}/reports/coverage.txt
+    DEPENDS ${coverage_reports}
+  )
+
+  add_custom_target(
+    coverage
+    DEPENDS ${PROJECT_BINARY_DIR}/reports/coverage.txt
+    COMMENT "Running all Catch2 tests with coverage"
+  )
+
+endif()
